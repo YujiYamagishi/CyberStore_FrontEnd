cadae710dad2f789389ea0272b031df19d3ae5e2
YujiYamagishi <yujiyamagishi1@gmail.com>
2025-08-28 14:16:55 -0300

    fix: created 'pages' folder for better project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
 
-import Navbar from './Components/navbar'
-import Hero from './Components/home/hero'
-import Browse from './Components/home/browse'
-import Suggest from './Components/home/suggest'
-import ProductsNavigation from './Components/home/productsnavigation'
-import DiscountProducts from './Components/home/discountproducts'
-import BigSummer from './Components/home/bigsummer'
-import Footer from './Components/footer'
-import '../src/styles/index.css'
+import Home from '../src/pages/home'
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
-      <Navbar />
+     
       <main className="pt-20 px-4">
-        <Hero />
-        <Browse/>
-        <Suggest/>
-        <ProductsNavigation/>
-        <DiscountProducts/>
-        <BigSummer/>
-        <Footer/>
+        <Home/>
       </main>
     </div>
   );
