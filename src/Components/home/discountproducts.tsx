b006555f7b83dbe2595f57ee5ab39481b12b82fb
Tarcisio Lucas <tarcisiolucas43@gmail.com>
2025-09-01 15:28:48 -0300

    Feat: discount products json

--- src/Components/home/discountproducts.tsx
+++ src/Components/home/discountproducts.tsx
@@ -1,10 +1,5 @@
-import React from 'react';
-
-import iphone14gold from '../../assets/iphone14gold.png';
-import airpods from '../../assets/airpods.png';
-import watch from '../../assets/applewatch.png';
-import iphone14white from '../../assets/iphone14white.png';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 type Product = {
   id: number;
@@ -13,34 +8,16 @@ type Product = {
   image: string;
 };
 
-const discountProducts: Product[] = [
-  {
-    id: 1,
-    title: 'Apple iPhone 14 Pro 512GB Gold',
-    price: '$900',
-    image: iphone14gold,
-  },
-  {
-    id: 2,
-    title: 'AirPods Max Silver',
-    price: '$2535',
-    image: airpods,
-  },
-  {
-    id: 3,
-    title: 'Apple Watch Series 9 GPS 41mm',
-    price: '$399',
-    image: watch,
-  },
-  {
-    id: 4,
-    title: 'Apple iPhone 14 Pro 1TB Gold (Midnight)',
-    price: '$549',
-    image: iphone14white,
-  },
-];
-
 export default function DiscountProducts() {
+  const [discountProducts, setDiscountProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch("/discountProducts.json")
+      .then((res) => res.json())
+      .then((data) => setDiscountProducts(data))
+      .catch((err) => console.error("Error loading products:", err));
+  }, []);
+
   return (
     <section className="discount-section">
       <h2 className="discount-title">Discounts up to -50%</h2>
@@ -49,7 +26,11 @@ export default function DiscountProducts() {
         {discountProducts.map((product) => (
           <div className="discount-card" key={product.id}>
             <div className="discount-image-container">
-              <img src={product.image} alt={product.title} className="discount-img" />
+              <img
+                src={product.image}
+                alt={product.title}
+                className="discount-img"
+              />
               <span className="heart">♡</span>
             </div>
             <p className="discount-title-text">{product.title}</p>
