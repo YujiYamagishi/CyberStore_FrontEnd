dc46d954bd1f5642449b4b02d8d451b46e85513e
Tarcisio Lucas <tarcisiolucas43@gmail.com>
2025-09-13 14:43:19 -0300

    refactor: use standard Card component in RelatedProducts and randomize product display

--- src/Components/productDetails/RelatedProducts.tsx
+++ src/Components/productDetails/RelatedProducts.tsx
@@ -1,29 +1,17 @@
-import { Link } from "react-router-dom";
-import { Heart } from "lucide-react";
-
+import ProductCard from "../card";
 export default function RelatedProducts({ products }: { products: any[] }) {
   return (
     <section className="related-products-section">
       <h2 className="section-title">Related Products</h2>
       <div className="related-products-grid">
         {products.slice(0, 4).map((product) => (
-          <div className="product-card" key={product.id}>
-            <div className="related-product-header">
-              <Heart size={32} className="heart-icon" />
-            </div>
-            <img
-              src={product.url_image}
-              alt={product.name}
-              className="product-img"
-            />
-            <h4 className="product-title" title={product.name}>
-              {product.name}
-            </h4>
-            <p className="product-price">${product.price}</p>
-            <Link to={`/product/${product.id}`}>
-              <button className="buy-button">Buy Now</button>
-            </Link>
-          </div>
+          <ProductCard
+            key={product.id}
+            id={product.id}
+            title={product.name}
+            price={product.price}
+            image={product.url_image}
+          />
         ))}
       </div>
     </section>
