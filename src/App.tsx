4a8267b0f63513fbc4ae69f3ed9fd565f56e395b
YujiYamagishi <yujiyamagishi1@gmail.com>
2025-09-15 14:05:42 -0300

    fix: return page home

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react'; 
 import Home from './pages/home';
 import ProductDetails from './pages/ProductDetails';
 import ProductsPage from './pages/productsPage';
@@ -7,9 +8,15 @@ import Footer from './Components/footer';
 
 
 function App() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col">
-      <Navbar  />
+      <Navbar />
       <main className="pt-20 flex-grow">
         <Routes>
           <Route path="/" element={<Home />} />
